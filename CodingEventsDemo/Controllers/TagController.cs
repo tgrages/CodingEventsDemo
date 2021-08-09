6ca3bc3ccbfceaee861e869af7af3f1df93a467d
Tom Grages <tgrages_12@hotmail.com>
2021-08-09 14:57:00 -0400

    paused eventTag model video

---
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -0,0 +1,25 @@
+﻿using CodingEventsDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Controllers
+{
+	public class TagController : Controller
+	{
+		private EventDbContext context;
+
+		public TagController(EventDbContext dbContext)
+		{
+			context = dbContext;
+		}
+	
+		public IActionResult Index()
+		{
+			List<TagController> tags = context.Tags.ToList();
+			return View(tags);
+		}
+	}
+}
--- CodingEventsDemo/Models/Event.cs
+++ CodingEventsDemo/Models/Event.cs
@@ -6,13 +6,10 @@ namespace CodingEventsDemo.Models
     public class Event
     {
         public string Name { get; set; }
-
         public string Description { get; set; }
-
         public string ContactEmail { get; set; }
-
-        public EventType Type { get; set; }
-
+        public EventCategory Category { get; set; }
+        public int CategoryId { get; set; }
         public int Id { get; set; }
 
         public Event(string name, string description, string contactEmail)
